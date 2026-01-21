3ebf37c8a7d3db28d64ea28fb8127cb6957b6899
Eh1z <godsentehiz11@gmail.com>
2026-01-21 14:21:31 +0100

    chore: update dependencies and devDependencies in package.json
    - Changed versioning for @react-three/fiber, three, and three-stdlib to fixed versions.
    - Added @react-three/postprocessing and leva as new dependencies.
    - Updated @types/node to version 25 in devDependencies.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Canvas } from "@react-three/fiber";
+import {Canvas}  from "@react-three/fiber";
 import { Experience } from "@/components";
 import { SoftShadows } from "@react-three/drei";
 import { Suspense } from "react";
@@ -11,11 +11,9 @@ const App = () => {
       <Canvas shadows camera={{ position: [30, 30, 0], fov: 30 }} className="w-full h-svh">
         <color attach="background" args={["#e3efef"]} />
         <SoftShadows size={32} />
-        <Suspense fallback={null}>
-          <Physics>
+     
             <Experience />
-          </Physics>
-        </Suspense>
+        
       </Canvas>
 
   );
